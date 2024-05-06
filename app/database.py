536080128cb0bde9c663938d06d83be3a674d7ba
JakubRudy <yobomedas@Jaskier>
2024-05-06 23:12:46 +0200

    added mysql db to project

---
+++ app/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine("mysql+pymysql://root:123@db:3306/satlab")
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,21 +1,23 @@
-
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy import text
+from database import session
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.templates = Jinja2Templates(directory="templates")
+
+session.execute(text("SELECT 1")) # szybki test
 
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse(
+    return app.templates.TemplateResponse(
         request=request, name="home.html"
     )
 
 @app.get("/informacje")
 def about(request: Request):
-    return templates.TemplateResponse(
+    return app.templates.TemplateResponse(
         request=request, name="about.html"
     )
