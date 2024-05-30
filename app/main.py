355465389295bda921240f814bf65ce8ed0b7ad0
Maksymilian Walicki <mwalicki@student.agh.edu.pl>
2024-05-30 14:35:30 +0200

    Add - home page

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.templates = Jinja2Templates(directory="templates")
 
-session.execute(text("SELECT 1")) # szybki test
+# session.execute(text("SELECT 1")) # szybki test
 
 @app.get("/")
 def home(request: Request):
@@ -21,3 +21,6 @@ def about(request: Request):
     return app.templates.TemplateResponse(
         request=request, name="about.html"
     )
+
+import uvicorn
+uvicorn.run(app, host="localhost", port=8000)
