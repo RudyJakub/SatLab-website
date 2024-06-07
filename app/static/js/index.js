eb93eea6527e53e6023b0eeba95823ef81101ca9
Maksymilian Walicki <mwalicki@student.agh.edu.pl>
2024-06-07 16:24:22 +0200

    Add -tailwind, carousel buttons, scroll animation, Fix - menu

---
+++ app/static/js/index.js
@@ -0,0 +1,27 @@
+// koncept zasad DRY tutaj leży
+
+$(window).on("load",function() {
+    let current_slide = 1;
+    let slide_count = 3;
+    $(".arrow-right").on("click", () => {
+        $(`.slider-nav a:nth-child(${current_slide})`).css("opacity", "0.75");
+        ++current_slide;
+        if(current_slide > slide_count) {
+            current_slide = 1;
+        }
+
+        window.location.hash = `#slide-${current_slide}`;
+        $(`.slider-nav a:nth-child(${current_slide})`).css("opacity", "1");
+    })
+
+    $(".arrow-left").on("click", () => {
+        $(`.slider-nav a:nth-child(${current_slide})`).css("opacity", "0.75");
+        --current_slide;
+        if(current_slide < 1) {
+            current_slide = slide_count;
+        }
+        
+        window.location.hash = `#slide-${current_slide}`;
+        $(`.slider-nav a:nth-child(${current_slide})`).css("opacity", "1");
+    })
+});
---
+++ app/static/js/slider.js
@@ -0,0 +1,36 @@
+// prosta implementacja fadeIn/Out podczas scrolla
+
+
+// config
+const bottomOffset = 200; // callable fadeIn/Out offset from bottom in px
+const fadeOffset = 10; // 10px in one direction
+
+function fade() {
+    var windowBottom = $(this).scrollTop() + $(this).outerHeight();
+    $(".fade").each(function() {
+
+      // Check the location of each desired element 
+      var objectBottom = $(this).offset().top + $(this).innerHeight()/2 + bottomOffset;
+      var direction = $(this).attr('direction') == 'left' ? `translateX(-${fadeOffset}px)` : `translateX(${fadeOffset}px)`; //
+      
+      // If the element is completely within bounds of the window, fade it in 
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {
+          $(this).css('opacity', '1').css('transform', 'translateX(0)');
+        }
+        
+      } else { //=object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).css('opacity', '0').css('transform', direction);}
+      }
+    });
+}
+
+
+// FADE ON SCROLL
+$(window).on("load",function() {
+    fade();
+    $(window).scroll(function() {
+        fade();
+    }).scroll(); //invoke scroll-handler on page-load
+});
+
